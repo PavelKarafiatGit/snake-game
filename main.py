27f6265936593503d3fda5b1cf831411990761a5
PavelKarafiat <pavelkarafiat.git@gmail.com>
2023-02-21 12:02:00 +0100

    main + tracer, screen settings

---
+++ main.py
@@ -0,0 +1,9 @@
+from turtle import Turtle, Screen
+
+screen = Screen()
+screen.bgcolor("grey")
+screen.title("Snake Game")
+screen.setup(width=600, height=600)
+screen.tracer(False)
+
+screen.exitonclick()
